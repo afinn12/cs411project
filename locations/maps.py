5aa346143697f460fb94cf519b2bc1f55d95ad3d
Tristan Lee <tristanlee73@gmail.com>
2023-11-12 17:41:01 -0500

    start on google maps api part: complete get route and compute split points

---
+++ locations/maps.py
@@ -0,0 +1,59 @@
+import requests
+import googlemaps_key
+import json
+
+GOOGLEMAPS_API_KEY = googlemaps_key.googlemaps_key
+
+GOOGLEMAPS_DIRECTIONS_API_ENDPOINT = 'https://maps.googleapis.com/maps/api/directions/json'
+
+DAILY_DISTANCE_LIMIT = 160900 # meters, equal to 100 miles
+
+
+# origin and destination are a tuple of lat-long coordinates
+def get_route(origin, destination):
+    route_args = {
+        'origin': f'{origin[0]},{origin[1]}',
+        'destination': f'{destination[0]},{destination[1]}',
+        'key': GOOGLEMAPS_API_KEY
+    }
+    response = requests.get(GOOGLEMAPS_DIRECTIONS_API_ENDPOINT, params=route_args)
+    data = response.json()
+
+    if response.status_code == 200 and data['status'] == 'OK':
+        return data['routes'][0]
+    else:
+        print(f"Error: {response.status_code} - {data.get('error_message', 'Unknown error')}")
+
+# route is route computed by get_route: it should contain just 1 leg
+def compute_split_points_on_daily_limit(route):
+    # extract the list of steps
+    steps = route['legs'][0]['steps']
+    split_points = []
+    distances = []
+    curr_distance = 0
+
+    for step in steps:
+        if curr_distance == 0:
+            curr_distance += step['distance']['value']
+        else:
+            # does adding yourself push the distances over the threshold?
+            if curr_distance+step['distance']['value']>DAILY_DISTANCE_LIMIT:
+                distances.append(curr_distance)
+                split_points.append((step['start_location']['lat'], step['start_location']['lng']))
+                curr_distance = 0
+            curr_distance += step['distance']['value']
+
+    distances.append(curr_distance)
+    return split_points, distances
+
+
+if __name__ == '__main__':
+    # # nyc coors
+    # origin = 40.7128, -74.0060
+    # # boston coors
+    # destination = 42.3601, -71.0589
+    # f = open('sample_route.txt', 'w')
+    # json.dump(get_route(origin, destination), f)
+    
+    route = json.load(open('sample_route.txt'))
+    print(compute_split_points_on_daily_limit(route))
