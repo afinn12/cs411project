ddfb12a0645a451fca3544b63ed43955cdc6f727
Tristan Lee <tristanlee73@gmail.com>
2023-12-11 22:14:49 -0500

    add actual API call instead of just sample; change some styles (loading wheel when API call is being made)

--- cs411project_app/static/cs411project_app/get_route.js
+++ cs411project_app/static/cs411project_app/get_route.js
@@ -1,22 +1,49 @@
 function get_sample_roadtrip_output() {
-  const apikey_endpoint = 'http://127.0.0.1:8000/cs411project_app/get_sample_roadtrip/';
-  const post_request_options = {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      }
-  };
+    const apikey_endpoint = 'http://127.0.0.1:8000/cs411project_app/get_sample_roadtrip/';
+    const post_request_options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
 
-  // Use fetch to make an HTTP request
-  return fetch(apikey_endpoint, post_request_options)
-      .then(response => response.json())
-      .then(data => {
-          return data;
-      })
-      .catch(err => {
-          console.error('Error reading the file:', err);
-          throw err;
-      });
+    // Use fetch to make an HTTP request
+    return fetch(apikey_endpoint, post_request_options)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error('Error reading the file:', err);
+            throw err;
+        });
+}
+
+function get_roadtrip_output(origin, destination, is_direct_route, distance_limit, start_date) {
+    const apikey_endpoint = 'http://127.0.0.1:8000/cs411project_app/get_roadtrip/';
+    const data = {
+        'origin': origin,
+        'destination': destination,
+        'is_direct_route': is_direct_route,
+        'distance_limit': distance_limit,
+        'start_date': start_date
+    }
+    const post_request_options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    };
+    return fetch(apikey_endpoint, post_request_options)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            console.error('Error making the API call:', err);
+            throw err;
+        });
 }
 
 // Example usage
